966a7823153c1c0a10ddeb7c3c547e9a9f364438
Anton Ignatov <abietis@gmail.com>
2016-07-07 01:01:12 +0300

    Configured proper build + initial page

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import ATV from 'atvjs/src/index';
+
+ATV.Page.create({
+	name: 'home',
+	template(data) {
+		return `<document>
+			<alertTemplate>
+				<title>${data.title}</title>
+				<description>${data.description}</description>
+			</alertTemplate>
+		</document>`;
+	},
+	data: {
+		title: 'Homepage',
+		description: 'Test page.'
+	}
+});
+
+ATV.start({
+	onLaunch(options) {
+		ATV.Navigation.navigate('home');
+	}
+});
