c4c775a02b1885c48bd8beef33ff70998f214419
Anton Ignatov <abietis@gmail.com>
2016-07-10 01:49:51 +0300

    Added more debugging info

--- src/index.js
+++ src/index.js
@@ -4,22 +4,38 @@ import * as TVDML from 'tvdml';
 
 TVDML
 	.handleRoute(TVDML.route.LAUNCH)
-	.pipe(TVDML.render(
+	.pipe(log('Route matched'))
+	.pipe(TVDML.render(({navigation: {BASEURL: url}}) => (
 		<document>
 			<alertTemplate>
 				<title>
-					Test pop
+					Demo application
 				</title>
 				<description>
-					Description
+					Launched on "{url}"
 				</description>
 				<button>
 					<text>Go to Next Page</text>
 				</button>
-				<button>
-					<text>Close</text>
-				</button>
+				<text>
+					For testing purposes
+				</text>
 			</alertTemplate>
 		</document>
-	))
-	.pipe(TVDML.pushDocument());
+	)))
+	.pipe(log('Document rendered'))
+	.pipe((payload) => {
+		for (let i = 0; i < payload.document.childNodes.length; i++) {
+			console.log(payload.document.childNodes.item(i).outerHTML);
+		}
+		return payload;
+	})
+	.pipe(TVDML.pushDocument)
+	.pipe(log('Document pushed to navigation'))
+
+function log(message = '') {
+	return (payload) => {
+		console.log(message, payload);
+		return payload;
+	}
+}
