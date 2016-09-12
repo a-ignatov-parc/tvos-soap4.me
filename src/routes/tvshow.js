8b3d766f3d1211d2708a4a448a89d09ea1f85652
Anton Ignatov <abietis@gmail.com>
2016-09-13 00:19:14 +0300

    Bugfix

--- src/routes/tvshow.js
+++ src/routes/tvshow.js
@@ -178,7 +178,7 @@ export default function() {
 				let showTrailerBtn = (
 					<buttonLockup onSelect={this.onShowTrailer}>
 						<badge src="resource://button-preview" />
-						<title>Show{'\n'}Trailer</title>
+						<title>Show Trailer</title>
 					</buttonLockup>
 				);
 
