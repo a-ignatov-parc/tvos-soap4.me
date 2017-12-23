a598046aa390ebcd9970ca4d109cf8a7529adfc3
Anton Ignatov <abietis@gmail.com>
2017-12-23 05:01:34 +0300

    Implemented react.js based tvdml proof-of-concept

--- src/index.js
+++ src/index.js
@@ -1,107 +1,71 @@
-/* global sessionStorage */
-
 import * as TVDML from 'tvdml';
 
-import * as user from './user';
-import { processFamilyAccount } from './user/utils';
+import lifecycle from 'recompose/lifecycle';
 
-import { get as i18n } from './localization';
-import { checkSession } from './request/soap';
+import { link } from './utils';
 
-import myRoute from './routes/my';
-import allRoute from './routes/all';
-import menuRoute from './routes/menu';
-import userRoute from './routes/user';
-import actorRoute from './routes/actor';
-import seasonRoute from './routes/season';
-import tvShowRoute from './routes/tvshow';
-import searchRoute from './routes/search';
-import genresRoute from './routes/genres';
-import settingsRoute from './routes/settings';
-import speedTestRoute from './routes/speedtest';
+function Hello(props) {
+  const name = props.name || 'Human';
+  const counter = props.counter || 0;
 
-import { AUTH, GUEST } from './routes/menu/constants';
+  return (
+    <document>
+      <head>
+        <style>{`
+          .title {
+            tv-text-style: title1;
+          }
+        `}</style>
+      </head>
+      <alertTemplate>
+        <title class='title'>Hello {name}!</title>
+        <description>Nice to see you 😸</description>
+        <button onSelect={link('next-page')}>
+          <text>🎉</text>
+        </button>
+        <button>
+          <text>🍸</text>
+        </button>
+        <text>And a small counter for your pleasure!</text>
+        <text style='tv-text-style: title2'>
+          {counter}
+        </text>
+      </alertTemplate>
+    </document>
+  );
+}
 
-import Loader from './components/loader';
+const HelloCounter = lifecycle({
+  componentWillMount() {
+    this.setState({ counter: 0 });
+  },
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({ counter: this.state.counter + 1 });
+    }, 5000);
+  },
+
+  componentWillUnmount() {
+    this.timer && clearInterval(this.timer);
+  },
+})(Hello);
 
 TVDML
   .subscribe(TVDML.event.LAUNCH)
-  .pipe(params => {
-    /**
-     * TODO: Need to save initial params in a better way then
-     * using `sessionStorage`. Maybe some in-memory storage.
-     */
-    sessionStorage.setItem('startParams', JSON.stringify(params));
-    return TVDML.navigate('get-token');
-  });
+  .pipe(TVDML.renderReact(payload => (
+    <HelloCounter name='Developer' />
+  )));
+
 
 TVDML
-  .handleRoute('get-token')
-  .pipe(TVDML.render(<Loader title={i18n('auth-checking')} />))
-  .pipe(checkSession)
-  .pipe(payload => {
-    const { logged, token, till } = payload;
-    user.set({ logged, token, till });
-    return payload;
-  })
-  .pipe(({ login }) => processFamilyAccount(login))
-  .pipe(() => TVDML.redirect('main'));
-
-TVDML
-  .handleRoute('main')
-  .pipe(menuRoute([
-    {
-      route: 'search',
-    }, {
-      route: 'my',
-      active: AUTH,
-      hidden: GUEST,
-    }, {
-      route: 'all',
-      active: GUEST,
-    }, {
-      route: 'genres',
-    }, {
-      route: 'settings',
-    },
-  ]));
-
-TVDML
-  .handleRoute('my')
-  .pipe(myRoute());
-
-TVDML
-  .handleRoute('all')
-  .pipe(allRoute());
-
-TVDML
-  .handleRoute('search')
-  .pipe(searchRoute());
-
-TVDML
-  .handleRoute('settings')
-  .pipe(settingsRoute());
-
-TVDML
-  .handleRoute('tvshow')
-  .pipe(tvShowRoute());
-
-TVDML
-  .handleRoute('season')
-  .pipe(seasonRoute());
-
-TVDML
-  .handleRoute('actor')
-  .pipe(actorRoute());
-
-TVDML
-  .handleRoute('speedtest')
-  .pipe(speedTestRoute());
-
-TVDML
-  .handleRoute('user')
-  .pipe(userRoute());
-
-TVDML
-  .handleRoute('genres')
-  .pipe(genresRoute());
+  .handleRoute('next-page')
+  .pipe(TVDML.renderReact(payload => (
+    <document>
+      <loadingTemplate>
+        <activityIndicator>
+          <title style='tv-text-style: title1'>💃</title>
+        </activityIndicator>
+      </loadingTemplate>
+    </document>
+  )));
